c171992a53c3dac521cd0854b397a0ffeb1e2cd4
bensarr <bensarr37@gmail.com>
2021-04-10 13:45:22 +0000

    Completer profil Entreprise Partie Font & Back

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Entreprise;
 use App\Entity\SecteurActivite;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,9 +37,52 @@ class EntrepriseController extends AbstractController
 
     /**
      * @Route("/entreprise/edit", name="app_entreprise_edit")
+     * @param Request $request
+     * @param string $uploadDir
+     * @param FileUploader $uploaderFile
+     * @return Response
      */
-    public function profil(): Response
+    public function profil(Request $request, string $uploadDir, FileUploader $uploaderFile): Response
     {
-        return $this->redirectToRoute('welcom');
+        if($request->isMethod("POST")) {
+            if ($this->isCsrfTokenValid('entreprise', $request->request->get('entreprise_token'))) {
+                $entreprise = new Entreprise();
+                if ($request->request->get('denomination') != '' && $request->request->get('telephone') != '' && $request->request->get('email') != '' && $request->request->get('adresse') != '' && $request->request->get('description') != '') {
+                    $entreprise->setNom($request->request->get('denomination'));
+                    $entreprise->setEmail($request->request->get('email'));
+                    $entreprise->setAdresse($request->request->get('adresse'));
+                    $entreprise->setTelephone($request->request->get('telephone'));
+                    $entreprise->setLogo($request->request->get('logo'));
+                    $entreprise->setDescription($request->request->get('description'));
+                    $entreprise->setUser($this->getUser());
+                    $secteursId=$request->request->get('secteurs');
+                    foreach ($secteursId as $s) {
+                        $entreprise->addSecteur($this->secteurRepository->find($s));
+                    }
+
+                    if (!empty($request->files->get('logo'))) {
+                        $logoFile = $request->files->get('logo');
+
+                        //Recuperation du nom du fichier
+                        $logoname = $logoFile->getClientOriginalName();
+
+                        //On appelle le service de chargement du fichier dans le repertoir specifié
+                        $ok = $uploaderFile->upload($uploadDir, $logoFile, $logoname);
+
+                        //On teste si le fichier est bien chargé
+                        if (!$ok) {
+                            return $this->redirectToRoute('app_entreprise_edit');
+                        } else {
+                            $entreprise->setLogo($logoname);
+                        }
+                    }
+                    $this->em->persist($entreprise);
+                    $this->em->flush();
+                    return $this->redirectToRoute('welcom');
+                }
+
+            }
+        }
+
     }
 }
